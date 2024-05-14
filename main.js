45f7adc9e6f73e9f3c1993592bb2b7a67d83fe6f
AsadullahNagori <164639593+AsadullahNagori@users.noreply.github.com>
2024-05-14 18:44:30 +0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,8 @@
+"use strict";
+//storing freaind names in & messige in Array
+let freaindnames = ["Asad", "Aliyan", "Saad", "Muzaifer", "Sami"];
+console.log(`Assalamwallikum, ${freaindnames[0]}`);
+console.log(`Assalamwallikum, ${freaindnames[1]}`);
+console.log(`Assalamwallikum, ${freaindnames[2]}`);
+console.log(`Assalamwallikum, ${freaindnames[3]}`);
+console.log(`Assalamwallikum, ${freaindnames[4]}`);
---
+++ main.ts
@@ -0,0 +1,7 @@
+//storing freaind names in & messige in Array
+let freaindnames :string[]=["Asad","Aliyan","Saad","Muzaifer","Sami"];
+console.log(`Assalamwallikum, ${freaindnames[0]}`);
+console.log(`Assalamwallikum, ${freaindnames[1]}`);
+console.log(`Assalamwallikum, ${freaindnames[2]}`);
+console.log(`Assalamwallikum, ${freaindnames[3]}`);
+console.log(`Assalamwallikum, ${freaindnames[4]}`);
